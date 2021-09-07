43b67bf9e89ef0f425f52d46284cf6b60d9cf86f
Muhammad Yusuf <muhammad.yusuf18010@student.unsika.ac.id>
2021-09-08 04:35:25 +0700

    Initial commit

---
+++ lib/MessageScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pesan extends StatelessWidget {
+  const Pesan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Menu Pesan"),
+    );
+  }
+}
